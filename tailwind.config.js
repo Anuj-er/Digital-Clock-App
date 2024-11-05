c9ea763d40105b136dcacbc713ed7ef8c167f773
Anuj Siwach <anujkumar142000@gmail.com>
2024-11-05 14:27:14 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,13 @@
-// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{html,js,jsx,ts,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}",
     "./public/index.html"
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
-};
+  plugins: [
+    require("tailwindcss-animate")
+  ],
+}
